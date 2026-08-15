072ee200da1ae6f88a07030b95a313b2db93211c
khushee <kt849669@gmail.com>
2026-08-15 12:51:24 +0530

    use new toppay supabase

--- user-app/js/config/supabase.js
+++ user-app/js/config/supabase.js
@@ -1,7 +1,7 @@
 // user-app/js/config/supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.3/+esm";
 
-const SUPABASE_URL = "https://ujzupmmvfrhpwziudydr.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_zqGOuDGYCAin5goXvlM5-Q_pliMNwlj";
+const SUPABASE_URL = "https://kxsgjfvtfmbruddeolbt.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable__asg5eO_X6CrsIp9DXO2bQ_H0Gr5c-j";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
